a2dc5916e1a84a6c00166ca9a0aca90c9932178e
shikiso <lewisemerson00@gmail.com>
2022-07-21 10:37:24 +0800

    added names and connection/disconnection messages

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+var nextID = 0;
+var users = {};
+
+function give_user_id(user){
+    
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/templates/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    io.emit('chat message', 'user connected', null);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('chat message', 'user disconnected', null);
+    });
+    socket.on('chat message', (msg, username) => {
+        io.emit('chat message', msg, username);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on port 3000');
+});
